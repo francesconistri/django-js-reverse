c8d8ead3b65ee2bebeff50171e49f5cba113170b
Francesco Nistri <francesconistri84@gmail.com>
2014-02-21 10:10:22 +0100

    change name of templatetag

--- django_js_reverse/templatetags/django_js_urls.py
+++ django_js_reverse/templatetags/django_js_urls.py
@@ -5,6 +5,6 @@ from django_js_urls.views import get_template_context
 register = Library()
 
 
-@register.inclusion_tag("django_js_reverse/urls_js.tpl", takes_context=True)
-def virtual_coins_widget(context):
+@register.inclusion_tag("django_js_reverse/urls_js.tpl")
+def django_urls():
     return get_template_context()
